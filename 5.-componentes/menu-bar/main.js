02f03c0ee84ee4131ca9179195664ebf0b459449
jonmolinna <jmolina2624@gmail.com>
2021-10-18 14:03:37 -0500

    Menu Bar

---
+++ 5.-componentes/menu-bar/main.js
@@ -0,0 +1,13 @@
+let scroll1 = window.pageYOffset;
+
+window.onscroll = function(){
+    let scroll2 = window.pageYOffset;
+
+    if(scroll1 > scroll2) {
+        document.querySelector("nav").style.top = "0";
+    } else {
+        document.querySelector("nav").style.top = "-100px";
+    }
+
+    scroll1 = scroll2
+}
